ccad03b84802d73afa8f56a7f9c4518dcb0d7e21
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-03-06 08:33:18 -0300

    feat(home): get api data on server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import { Movie } from '../types/Movie';
+import requests from '../utils/requests';
 
-const Home: NextPage = () => {
+interface HomeProps {
+  results: Movie[];
+}
+
+const Home = ({ results }: HomeProps) => {
   return (
     <div>
       <Head>
@@ -18,3 +24,19 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+};
