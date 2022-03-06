59cab3a5b990b479846d33c36569a77d9d4d1738
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-03-06 08:29:29 -0300

    feat(types): add movie type

---
+++ src/types/Movie.ts
@@ -0,0 +1,11 @@
+export type Movie = {
+  id: number;
+  original_name: string;
+  title: string;
+  overview: string;
+  backdrop_path: string;
+  poster_path: string;
+  release_date: string;
+  first_air_date: string;
+  vote_count: number;
+};
