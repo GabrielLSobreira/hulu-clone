4f990be4d0e149ef83c308e8269a9a4d4a53d293
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-03-05 22:44:48 -0300

    feat(components): create header and headerItem

---
+++ src/components/HeaderItem/index.tsx
@@ -0,0 +1,20 @@
+interface HeaderItemProps {
+  title: string;
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+function HeaderItem({ title, Icon }: HeaderItemProps) {
+  return (
+    <div
+      className="flex flex-col items-center cursor-pointer group
+  w-12 sm:w:20 hover:text-white"
+    >
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest">
+        {title}
+      </p>
+    </div>
+  );
+}
+
+export default HeaderItem;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Head>
+        <title>Hulu Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
